743d556b046a1bc93ebe175403e54080dcfbb64b
root005 <1726690348@qq.com>
2016-12-06 18:04:04 +0800

    新文件：   conf/app.conf 	新文件：   controllers/default.go 	新文件：   main.go 	新文件：   routers/router.go 	新文件：   tests/default_test.go 	新文件：   views/index.tpl

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	_ "company_pro/routers"
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	beego.Run()
+}
+
---
+++ routers/router.go
@@ -0,0 +1,10 @@
+package routers
+
+import (
+	"company_pro/controllers"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+    beego.Router("/", &controllers.MainController{})
+}
